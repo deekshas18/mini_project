a8a68a9e6665f6d5c3c63d869b00e57b6c48abe8
Anagha-N13 <anagha2003.13@gmail.com>
2024-06-04 05:54:25 +0530

    doctor routes added

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,11 @@ const env = require('dotenv');
 env.config({path:__dirname + '/env/.env'});
 const port = process.env.PORT;
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json())
+const doctorRoutes = require('./routes/doctorRoutes');
+app.use('/api/doctor-routes/',doctorRoutes);
+
 app.listen(port,() => {
     console.log("server running on port ", port);
 })
