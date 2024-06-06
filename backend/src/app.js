94842936a60a6d9804b487552b010d8daa2f14fd
Ananya <ananyasetty127@gmail.com>
2024-06-06 08:13:36 +0530

    patient routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,9 +2,19 @@ require('./db/mongoose');
 const express = require('express');
 const app = express();
 
+const bodyParser = require("body-parser");
+// Parse JSON bodies
+app.use(bodyParser.json());
+
 const env = require('dotenv');
 env.config({path:__dirname + '/env/.env'});
 const port = process.env.PORT;
+
+
+const patientRouter = require('./routes/patient')
+app.use("/api/patient", patientRouter)
+
+
 
 app.listen(port,() => {
     console.log("server running on port ", port);
