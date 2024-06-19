8470599267841b56dcf992e811ab5ad916200ece
deekshas18 <deekshas182003@gmail.com>
2024-06-19 15:04:50 +0530

    pushing frontend

--- frontend/src/pages/Display.jsx
+++ frontend/src/pages/Display.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function Display() {
   return (
-    <div>Display</div>
-  )
+    <div>
+      <div className="search-main">
+        <select className="select-field" id="doctorSelect">
+          <option value="">Select Doctor</option>
+          {/* Add options dynamically */}
+        </select>
+        <select className="select-field" id="patientSelect">
+          <option value="">Select Patient</option>
+          {/* Add options dynamically */}
+        </select>
+      </div>
+      <button className="search-btn">Search</button>
+      <div></div>
+    </div>
+  );
 }
 
-export default Display
+export default Display;
